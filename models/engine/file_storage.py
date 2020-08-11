27a8b4bfcb6bbff5d35eb8f4cba597b3bfbb1283
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-11 20:32:04 +0000

    Update class and documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,15 +2,37 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        objDict = {}
+        if cls is not None:
+            for key, obj in self.__objects.items():
+                if isinstance(obj, cls):
+                    objDict[key] = obj
+            return objDict
+        return self.__objects
+
+    def delete(self, obj=None):
+        """Deletes an object from __object dictionary if exist"""
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if (key, obj) in self.__objects.items():
+                self.__objects.pop(key)
+            self.save()
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -18,31 +40,23 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
-                  }
+                }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
--- tests/test_console.py
+++ tests/test_console.py
@@ -76,6 +76,10 @@ class testConsole(unittest.TestCase):
         pass
 
     def test_console_do_create(self):
+        """test_console_do_count
+
+        test secure instance creation
+        """
         HBNBCommand.do_create(
             self, 'Place city_id="0001" user_id="0001" name="My_little_house" \
             number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 \
