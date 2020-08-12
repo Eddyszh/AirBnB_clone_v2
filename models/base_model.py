06889045d8d47b34b9d058d9c207972b8b77cfaa
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-12 15:03:59 +0000

    Add imports

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 
@@ -14,6 +13,7 @@ class BaseModel:
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -23,22 +23,16 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "created_at":
+                if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == '__class__':
+                if key != '__class__':
                     setattr(self, key, value)
             if "id" not in kwargs.keys():
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs.keys():
-                setattr(self, "created_at", value)
+                self.created_at = datetime.now()
             if "updated_at" not in kwargs.keys():
-                setattr(self, "updated_at", value)
-            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-            #                                          '%Y-%m-%dT%H:%M:%S.%f')
-            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-            #                                          '%Y-%m-%dT%H:%M:%S.%f')
-            # del kwargs['__class__']
-            self.__dict__.update(kwargs)
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -57,11 +51,11 @@ class BaseModel:
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary:
-            del(dictionary["_sa_instance_state"])
+            del dictionary["_sa_instance_state"]
         return dictionary
 
     def delete(self):
