a2ed22656475d78850749e499d73ccc43d1acae2
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-17 21:21:06 +0000

    Check case 10

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Sets up web servers for the deployment of web_static
 if ! which nginx > /dev/null 2>&1; then
-  apt-get -y update
-  apt-get install -y nginx
+  sudo apt-get -y update
+  sudo apt-get install -y nginx
 fi
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
@@ -16,4 +16,4 @@ echo "<html>
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _;/ a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
-service nginx restart
+sudo service nginx restart
