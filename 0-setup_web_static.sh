844e642fc04b89230537b98cd8ed903ffa3495b7
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-17 19:44:06 +0000

    fix case 10

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ printf %s "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-service nginx restart
 sed -i "/server_name _;/ a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t} " /etc/nginx/sites-available/default
+service nginx restart
