89731d63c384ba46d7930967638ec0b92fd7e1a6
eddyszh <eddyszh@gmail.com>
2020-09-02 21:40:15 -0500

    Fix doc issue

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from flask import Flask
-"""starts a Flask web application"""
+"""starts a Flask web application
+"""
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """Displays Hello HBNB"""
     return 'Hello HBNB!'
