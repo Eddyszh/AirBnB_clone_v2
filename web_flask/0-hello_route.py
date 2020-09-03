4c0338625901c185fd2558989c17c6f7d3606664
eddyszh <eddyszh@gmail.com>
2020-09-02 21:47:05 -0500

    Fix route issue

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,12 @@ from flask import Flask
 """starts a Flask web application
 """
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """Displays Hello HBNB"""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
